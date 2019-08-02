1b3954984d11db57e5ee3b0ea5e28c9d53c5b663
Andy Meneely <andy@se.rit.edu>
2019-08-02 13:12:12 -0400

    I dub thee v1.5

---
+++ design/precache-manifest.996913b00193ac1e2ca65872ef4ab7e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4398792fdb6476e0c9dc857feee8c397",
+    "url": "/design/index.html"
+  },
+  {
+    "revision": "5d4108ad16ee47b01961",
+    "url": "/design/static/css/main.d0364152.chunk.css"
+  },
+  {
+    "revision": "b2b08333d3d7605cde39",
+    "url": "/design/static/js/2.79ac4a6c.chunk.js"
+  },
+  {
+    "revision": "5d4108ad16ee47b01961",
+    "url": "/design/static/js/main.ff4271da.chunk.js"
+  },
+  {
+    "revision": "91bbae28a0d604b40337",
+    "url": "/design/static/js/runtime~main.14fa30ac.js"
+  },
+  {
+    "revision": "13863394f931424ea78f2ac4ff2dc5c1",
+    "url": "/design/static/media/down.13863394.svg"
+  },
+  {
+    "revision": "5fcefbcb19f5b423e48b07bec9c2327c",
+    "url": "/design/static/media/left.5fcefbcb.svg"
+  },
+  {
+    "revision": "bf7c32452bc56f4d1a4b896c32d49e11",
+    "url": "/design/static/media/right.bf7c3245.svg"
+  },
+  {
+    "revision": "62967a3f851048eb7795e89e854c38b5",
+    "url": "/design/static/media/up.62967a3f.svg"
+  }
+]);
